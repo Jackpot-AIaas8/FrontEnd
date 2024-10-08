848c6c925d2817eef84de5f2df06dea08fdfb85b
김민준 <kimmj9703@gmail.com>
2024-10-08 18:30:33 +0900

    메인 페이지 캐러셀 수정

--- src/components/Main/MainCarousel.jsx
+++ src/components/Main/MainCarousel.jsx
@@ -1,7 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import mainCarousel1 from "../../static/mainCarousel1.jpg"
+// import mainCarousel1 from "../../static/mainCarousel1.jpg"
 import mainCarousel2 from "../../static/mainCarousel2.jpg"
 import mainCarousel3 from "../../static/mainCarousel3.jpg"
 import mainCarousel4 from "../../static/mainCarousel4.jpg"
@@ -22,9 +22,9 @@ function MainCarousel() {
     return (
         <div className="slider-container" style={{display: "block"}}>
             <Slider {...settings}>
-                <div>
+                {/* <div>
                     <img src={mainCarousel1} alt="" style={{height: '700px', width: '100%'}}/>
-                </div>  
+                </div>   */}
                 <div>
                     <img src={mainCarousel2} alt="" style={{height: '700px', width: '100%'}}/>
                 </div>
