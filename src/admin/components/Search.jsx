29d53d5650d7d6d9c7387c8ddc74b28a360cae0f
전세계 <kssjssk@gmail.com>
2024-10-10 17:30:30 +0900

    fix: search box label-named error

--- src/admin/components/Search.jsx
+++ src/admin/components/Search.jsx
@@ -1,45 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const Search = ({onSearch}) => {
-    const [searchText, setSearchText] = useState('');
+const Search = ({ onSearch }) => {
+  const [searchText, setSearchText] = useState("");
 
-    const handleInputChange = (e) => {
-        setSearchText(e.target.value);
-    };
+  const handleInputChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSearch(searchText);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(searchText);
+  };
 
-    return (
-        <StyledWrapper>
-            <form onSubmit={handleSubmit}>
-                <div className="InputContainer">
-                    {/*<form onSubmit={handleSubmit}>*/}
-                    <input
-                        placeholder="Search"
-                        id="searchText"
-                        className="input"
-                        name="searchText"
-                        type="text"
-                        value={searchText}
-                        onChange={handleInputChange}
-                    />
-                    <label className="labelforsearch" htmlFor="input">
-                        <button type="submit" style={{background: 'none', border: 'none'}}>
-                            <svg className="searchIcon" viewBox="0 0 512 512">
-                                <path
-                                    d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
-                            </svg>
-                        </button>
-                    </label>
-                    {/*</form>*/}
-                </div>
-            </form>
-        </StyledWrapper>
-    );
+  return (
+    <StyledWrapper>
+      <form onSubmit={handleSubmit}>
+        <div className="InputContainer">
+          {/*<form onSubmit={handleSubmit}>*/}
+          <input
+            placeholder="Search"
+            id="searchText"
+            className="input"
+            name="searchText"
+            type="text"
+            value={searchText}
+            onChange={handleInputChange}
+          />
+          <label className="labelforsearch" htmlFor="searchText">
+            <button
+              type="submit"
+              style={{ background: "none", border: "none" }}
+            >
+              <svg className="searchIcon" viewBox="0 0 512 512">
+                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+              </svg>
+            </button>
+          </label>
+          {/*</form>*/}
+        </div>
+      </form>
+    </StyledWrapper>
+  );
 };
 
 const StyledWrapper = styled.div`
@@ -105,7 +107,6 @@ const StyledWrapper = styled.div`
     background-color: rgb(255, 230, 230);
     transition-duration: 0.3s;
   }
-
 `;
 
 export default Search;
