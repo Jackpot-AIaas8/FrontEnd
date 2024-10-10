f8f4e906121077960840ccef6c6283165f3e43cc
전세계 <kssjssk@gmail.com>
2024-10-10 09:40:06 +0900

    git merge into segue

--- src/src/main/java/com/bitcamp/jackpot/repository/OrdersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bitcamp.jackpot.repository;
-
-import com.bitcamp.jackpot.domain.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders, Integer> {
-}
