2cad698d734c6a5d1d94eab795ad3e9d39bf3fd6
김명진 <111105992+KimMyeongJinn@users.noreply.github.com>
2024-10-15 15:13:35 +0900

    default.conf와 Constants.js 수정

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = "http://localhost:8181/";
-export const SERVER_URL = "http://10.0.2.6:8181/"; //NCP SERVER URL
+ export const SERVER_URL = "http://localhost:8181/";
+//export const SERVER_URL = "http://223.130.158.97/api/"; //NCP SERVER URL
