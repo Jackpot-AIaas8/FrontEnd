7c9ecb4b2ff30830e1953da98d301f7628d82685
전세계 <kssjssk@gmail.com>
2024-10-16 10:09:57 +0900

    mypage 수정 작업

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,43 +1,184 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import logoImage from "../static/newLogoverticalOrange.png";
-import MockMypageData from "../myPage/MockMypageData";
+
 import {
   StyledMypageWrapper,
-  StyledNavBar,
-  StyledProFileArea,
-  StyledMypageMenu,
   StyeldRightSection,
   StyledProfileContainer,
   StyledProfileInfo,
   StyledImage,
 } from "../myPage/Mypage.styles";
 
-import MenuItem from "../myPage/MenuItem";
 import { AuthContext } from "../token/AuthContext";
 import mockMypageData from "../myPage/MockMypageData";
 import apiClient from "../token/AxiosConfig";
 import { SERVER_URL } from "../config/Constants";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
+import MypageSideBar from "../myPage/MyPageSideBar";
+
+import {
+  MDBCol,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBTypography,
+} from "mdb-react-ui-kit";
+
+export function MDBui() {
+  return (
+    <MDBRow className="justify-content-center align-items-center h-100 w-100">
+      <MDBCol lg="6" className=" mb-lg-0 p-0 w-100">
+        <MDBCard className="flex" style={{ borderRadius: ".5rem" }}>
+          <MDBRow className="g-0">
+            <MDBCol
+              md="4"
+              className="flex flex-column text-black justify-center align-center"
+              style={{
+                borderTopLeftRadius: ".5rem",
+                borderBottomLeftRadius: ".5rem",
+              }}
+            >
+              <MDBCardImage
+                src="https://static.nid.naver.com/images/web/user/default.png"
+                alt="Avatar"
+                className="my-3"
+                style={{ width: "80px", borderRadius: "50%" }}
+                fluid
+              />
+              <MDBTypography tag="h5">전세계</MDBTypography>
+              <MDBCardText>Gold</MDBCardText>
+            </MDBCol>
+            <MDBCol md="8">
+              <MDBCardBody className="p-4">
+                <MDBTypography tag="h6">Information</MDBTypography>
+                <hr className="mt-0 mb-4" />
+                <MDBRow className="pt-1">
+                  <MDBCol size="6" className="mb-3">
+                    <MDBTypography tag="h6">Email</MDBTypography>
+                    <MDBCardText className="text-muted">
+                      이메일 || *******처리
+                    </MDBCardText>
+                  </MDBCol>
+                  <MDBCol size="6" className="mb-3">
+                    <MDBTypography tag="h6">Phone</MDBTypography>
+                    <MDBCardText className="text-muted">
+                      000-000-0000
+                    </MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+
+                <MDBTypography tag="h6">Information</MDBTypography>
+                <hr className="mt-0 mb-4" />
+                <MDBRow className="pt-1">
+                  <MDBCol size="6" className="mb-3">
+                    <MDBTypography tag="h6">닉네임</MDBTypography>
+                    <MDBCardText className="text-muted">nickName</MDBCardText>
+                  </MDBCol>
+                  <MDBCol size="6" className="mb-3">
+                    <MDBTypography tag="h6">address</MDBTypography>
+                    <MDBCardText className="text-muted">집 주소</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCol>
+          </MDBRow>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
+  );
+}
 
 const Mypage = () => {
-  const { logout, authState } = useContext(AuthContext);
-  const navigate = useNavigate(); // useNavigate 훅 사용
+  const { authState } = useContext(AuthContext);
+
   const [rank, setRank] = useState("브론즈");
-  const [nickName, setNickName] = useState();
+
   // MenuItems.section
-  const [currentPage, setCurrentPage] = useState("내프로필");
-  const menuItems = [
-    { href: "#1", text: "개인정보수정" },
-    { href: "/shop/findList", text: "구매 내역" },
-    { href: "#3", text: "취소/ 반품 / 환불 내역" },
-    { href: "#4", text: "1:1 문의 내역" },
-    { href: "#5", text: "펀딩내역" },
-    { href: "/signOut", text: "로그아웃", onClick: logout }, // 로그아웃 항목에 onClick 설정
-  ];
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   const [memberMerchList, setMemberMerchList] = useState([]);
   const [oneOnOneboardList, setoneOnOneboardList] = useState([]);
   const [dogList, setDogList] = useState([]);
-  const loggedInMemberId = authState?.member?.memberId || null;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [address, setAddress] = useState("");
+  const editUser = () => {
+    return (
+      <>
+        <>
+          <Dialog open={isDialogOpen} onClose={handleCloseDialog}>
+            <DialogTitle>개인정보 수정</DialogTitle>
+            <DialogContent>
+              <input
+                type="text"
+                placeholder="이메일 수정"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Button onClick={(e) => e.preventDefault()} color="primary">
+                이메일 중복검사
+              </Button>
+              <input
+                type="password"
+                placeholder="비밀번호 수정"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="전화번호 수정"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="이름 수정"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="닉네임 수정"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+              />
+              <Button onClick={(e) => e.preventDefault()} color="primary">
+                닉네임 중복검사
+              </Button>
+              <input
+                type="text"
+                placeholder="주소 수정"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog} color="primary">
+                닫기
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </>
+      </>
+    );
+  };
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
 
   useEffect(() => {
     const shopMerchList = async () => {
@@ -47,7 +188,6 @@ const Mypage = () => {
         });
         const memberMerchList = response.data.dtoList || [];
         setMemberMerchList(memberMerchList);
-        console.log(memberMerchList); // 여기 콘솔 찍는거 있음. 데이터가 잘 들어왔는지 봐야지
       } catch (error) {
         console.error("구매리스트 받아오기 실패:", error);
       }
@@ -96,103 +236,13 @@ const Mypage = () => {
     dogFund(); // 비동기 함수 호출
   }, []);
 
-  const handleMenuClick = (item) => {
-    if (item.onClick) {
-      item.onClick(); // 로그아웃 함수 호출
-    } else {
-      navigate(item.href); // 해당 페이지로 이동
-      setCurrentPage(item.text); // 현재 페이지 상태 업데이트
-    }
-  };
   return (
     <>
       <div className="container flex justify-center">
         <StyledMypageWrapper className="flex flex-row">
-          <StyledNavBar className="left-section flex align-center justify-center flex-column">
-            <div className="section-nav h-full flex flex-column">
-              <div className="logoArea flex flex-row justify-start align-center">
-                <a href="/" className="logo flex">
-                  <img
-                    className="logo-image"
-                    src={logoImage}
-                    alt="로고이미지"
-                    style={{ height: "50px", width: "auto" }}
-                  />
-                </a>
-                <a href="/myPage" className="logo_title flex">
-                  <h1 className="text">
-                    <span className="blind">MyPage</span>
-                  </h1>
-                </a>
-              </div>
-
-              <StyledProFileArea className="flex flex-column w-full align-center justify-center">
-                <div className="profile_inner">
-                  <a href="#" className="photo">
-                    <img
-                      src="https://static.nid.naver.com/images/web/user/default.png"
-                      alt="프로필 이미지"
-                    />
-                  </a>
-                  <div className="profile">
-                    <p className="name">{mockMypageData.member.name}</p>
-                    <p className="usemail">get Email@naver.com</p>
-                  </div>
-                </div>
-              </StyledProFileArea>
-
-              <StyledMypageMenu
-                role="menu"
-                className="flex flex-column align-start justify-center "
-              >
-                <ul className="menu_list flex flex-column">
-                  {/* nav바 버튼 반복 */}
-                  {menuItems.map((item) => (
-                    <MenuItem
-                      key={item.text}
-                      href={item.href}
-                      text={item.text}
-                      isActive={currentPage === item.text}
-                      onClick={() => handleMenuClick(item)}
-                    />
-                  ))}
-                </ul>
-              </StyledMypageMenu>
-            </div>
-          </StyledNavBar>
-
-          <StyeldRightSection className="right-section flex flex-column w-full ">
-            <div className="section-mypage flex align-start justify-start w-full">
-              <StyledProfileContainer className="flex flex-row justify-between w-full">
-                <div className="profile-details flex align-center">
-                  <a href="#2" onClick={() => alert("정보 수정")}>
-                    <StyledImage className="flex w-full">
-                      <img
-                        src="https://static.nid.naver.com/images/web/user/default.png"
-                        alt="프로필 이미지"
-                      />
-                    </StyledImage>
-                  </a>
-                  <StyledProfileInfo className="flex flex-column align-start">
-                    <p>{mockMypageData.member.nickName} 님 안녕하세요.</p>
-                    <p>
-                      당신의 등급은{" "}
-                      <span>
-                        <b>{rank}</b>
-                      </span>
-                      입니다.
-                    </p>
-                  </StyledProfileInfo>
-                </div>
-                <div className="point-view text-center flex align-center">
-                  <a href="/shop_mypage/?m2=point_list">
-                    <p>누적금액</p>
-                    <span>XXXX</span>
-                  </a>
-                </div>
-              </StyledProfileContainer>
-            </div>
-
+          <MypageSideBar />
+          <StyeldRightSection className="right-section flex flex-column w-full  align-center justify-center ">
+            <MDBui />
             <div className="section-mypage flex w-full flex-column">
               <h2 className="text-left p-2">구매내역</h2>
               <table>
@@ -304,7 +354,7 @@ const Mypage = () => {
                       <td>{fundDTO.amount} 원</td>
                     </tr>
                   ))}
-                  {/* {oneOnOneboardList.map((fundDTO) => (
+                  {/* {fund.map((fundDTO) => (
                               <tr key={fundDTO.fundId}>
                                 <td>{fundDTO.fundId}</td>
                                 <td>{fundDTO.dogId}</td>
