54c4bbdbaced9163355559ed22e6a9e51e155eaf
김민준 <kimmj9703@gmail.com>
2024-10-17 20:02:34 +0900

    pwdCheckModal

--- src/myPage/MypagePwModal.jsx
+++ src/myPage/MypagePwModal.jsx
@@ -24,6 +24,7 @@ const MypagePwModal = ({ open, onClose }) => {
 
       if (response.status === 200 && response.data === true) {
         navigate("/mypage");
+        onClose();
       } else {
         alert("비밀번호가 틀립니다.");
       }
@@ -32,29 +33,40 @@ const MypagePwModal = ({ open, onClose }) => {
       alert("비밀번호 확인에 실패했습니다.");
     } finally {
       setPassword("");
-      onClose();
     }
   };
 
   const handleClose = () => {
     setPassword("");
     onClose();
-    navigate(-1); //닫기 버튼 누르면 이전페이지로 가게 해놓음
+    navigate(-1); //뒤로가기 버튼 누르면 이전페이지로 가게 해놓음
   };
+
+  // // 버튼 배경 색상 바꾸는 변수
+  // const OrangeButton = styled(Button)(({ theme }) => ({
+  //   color: "#FFFFFF",
+  //   backgroundColor: "#ff7600",
+  //   "&:hover": {
+  //     backgroundColor: "#d64229",
+  //   },
+  // }));
 
   // 버튼 색상 바꾸는 변수
   const OrangeButton = styled(Button)(({ theme }) => ({
-    color: "#FFFFFF",
-    backgroundColor: "#ff7600",
+    color: "#ff7600", // 텍스트 색상
+    backgroundColor: "transparent", // 배경색 투명하게 설정
+    border: `2px solid #ff7600`, // 외곽선 색상
     "&:hover": {
-      backgroundColor: "#d64229",
+      backgroundColor: "#d64229", // hover 시 배경색
+      borderColor: "#d64229", // hover 시 외곽선 색상
+      color: "#ffffff",
     },
   }));
 
   // 이건 그 입력창 아웃라인 색상 주황색으로 바꾸는거
   const OrangeTextField = styled(TextField)(({ theme }) => ({
     "& .MuiInputLabel-root": {
-      color: "#ff7600",
+      color: "#000000",
     },
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
@@ -74,11 +86,19 @@ const MypagePwModal = ({ open, onClose }) => {
     color: "#ff7600",
   }));
 
+  // 모달창 자체 외곽라인 색상 바꾸기
+  const OrangeDialog = styled(Dialog)(({ theme }) => ({
+    "& .MuiDialog-paper": {
+      border: "2px solid #ff7600", // 외곽선 색상 설정
+      borderRadius: "10px", // 둥근 모서리 설정
+    },
+  }));
+
   return (
-    <Dialog open={open} onClose={onClose}>
+    <OrangeDialog open={open} onClose={onClose}>
       <OrangeDialogTitle>
         <LockOutlinedIcon
-          style={{ marginRight: "8px", marginTop: "8px", color: "#ff7600" }}
+          style={{ marginRight: "8px", marginTop: "8px", color: "#000000" }}
         />
         비밀번호 확인
       </OrangeDialogTitle>
@@ -95,10 +115,10 @@ const MypagePwModal = ({ open, onClose }) => {
         />
       </DialogContent>
       <DialogActions>
-        <OrangeButton onClick={handleClose}>닫기</OrangeButton>
+        <OrangeButton onClick={handleClose}>뒤로가기</OrangeButton>
         <OrangeButton onClick={handleConfirm}>확인</OrangeButton>
       </DialogActions>
-    </Dialog>
+    </OrangeDialog>
   );
 };
 
