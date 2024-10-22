58a6515a34be219316656a7b6bd3fe37f0f99333
김명진 <111105992+KimMyeongJinn@users.noreply.github.com>
2024-10-22 17:31:44 +0900

    리버스 프록시

--- src/config/AuctionWebSocket.js
+++ src/config/AuctionWebSocket.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import io from 'socket.io-client';  // socket.io-client import
 
-const socket = io('http://223.130.158.97/socket.io/',
+const socket = io('http://223.130.158.97/socket.io/socket.io',
     {
         transports: ['websocket'],  // WebSocket으로만 연결 시도
         upgrade: false, // Long Polling으로 업그레이드 방지
